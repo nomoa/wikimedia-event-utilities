4e1021b40f573888d308ab6ac9eb9498fede5d19
Ottomata <aotto@wikimedia.org>
2021-02-19 16:21:03 -0500

    Fix TestEventSchemaValidator so it does not look up remote http url during test
    Change-Id: I97d975623386c23557d4e48d8d14c5ff44e7892c

--- eventutilities/src/test/java/org/wikimedia/eventutilities/core/event/TestEventSchemaValidator.java
+++ eventutilities/src/test/java/org/wikimedia/eventutilities/core/event/TestEventSchemaValidator.java
@@ -1,5 +1,7 @@
 package org.wikimedia.eventutilities.core.event;
 
+import java.io.File;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.wikimedia.eventutilities.core.json.JsonLoadingException;
@@ -7,20 +9,26 @@ import org.wikimedia.eventutilities.core.json.JsonLoadingException;
 import com.github.fge.jsonschema.core.exceptions.ProcessingException;
 import com.github.fge.jsonschema.core.report.ProcessingReport;
 
+
 public class TestEventSchemaValidator {
-    private final EventSchemaLoader loader = new EventSchemaLoader(WikimediaDefaults.SCHEMA_BASE_URIS);
+    private final EventSchemaLoader loader = new EventSchemaLoader(
+        "file://" + new File("src/test/resources/event-schemas/repo4").getAbsolutePath()
+    );
     private final EventSchemaValidator validator = new EventSchemaValidator(loader);
 
     @Test
     public void testValidEventMustSucceedValidation() throws ProcessingException, JsonLoadingException {
-        String simpleEvent = "{\"$schema\": \"/fragment/common/1.0.0\", \"meta\": {\"dt\": \"2020-12-20T20:00:00Z\", \"stream\": \"test.stream\"}}";
+        String simpleEvent = "{\"$schema\": \"/test/event/1.0.0\", \"meta\": { \"stream\": " +
+            "\"test.event.example\", \"dt\": \"2019-01-01T00:00:00Z\" }, \"dt\": " +
+            "\"2019-01-01T00:00:00Z\", \"test\": \"specific test value\", \"test_map\": " +
+            "{ \"key1\": \"val1\", \"key2\": \"val2\" }}";
         Assertions.assertTrue(validator.validate(simpleEvent).isSuccess());
     }
 
     @Test
     public void testInvalidEventMustFailsValidation() throws ProcessingException, JsonLoadingException {
-        String brokenEvent = "{\"$schema\": \"/fragment/common/1.0.0\"}";
-        ProcessingReport report = validator.validate(brokenEvent);
+        String invalidEvent = "{\"$schema\": \"/test/event/1.0.0\", \"dt\": \"2019-01-01T00:00:00Z\"}";
+        ProcessingReport report = validator.validate(invalidEvent);
         Assertions.assertTrue(report.toString().contains("missing: [\"meta\"]"));
     }
 }
